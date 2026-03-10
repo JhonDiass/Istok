cb9a35f67be34b6db10d96d24e5e1e705156aa64
Jhonatan Dias <129805707+JhonDiass@users.noreply.github.com>
2026-03-09 23:51:17 -0300

    Refatoracao de código
    Código refatorado com correções, novas verificações e novas funções.
    
    - Foi separado as verificações das função que realiza o cadastramento do novo produto, criando funções especificas para cada passo do cadastramento.
    - Foi criado verificações de caracteres corretos durante a digitação do valor, quantidade e código do produto, evitando que seja realizado entrada de caractere em um campo numeral, quebrando o funcionamento do programa.
    - Corrigido a função de checagem de códigos já cadastrados, evitando que toda vez que um novo produto fosse cadastrado, os códigos de barras fossem duplicados dentro da lista verificadora.

--- Estoque_de_mercado.py
+++ Estoque_de_mercado.py
@@ -4,64 +4,81 @@ produtos_Cadastrados = [
     {"nome": "macarrao", "codigo": 7896022200213 , "valor": 3.49, "quantidade": 19},
     {"nome": "leite", "codigo": 7896256600223 , "valor": 3.49, "quantidade": 24}
 ]
-
+codigos_verificados = []
 print("Olá! Bem vindo ao seu Istok!")
 
-# funcao calcular 
+# funcoes 
 
 def calcular_Total():
-    total_Itens = []
-
+    total_Itens = 0
+    
     # percorrendo a lista de produtos cadastrados.
     for produto in produtos_Cadastrados: 
-        
-        # Adicionando especificamente a "quantidade" na variavel total_itens.
-        total_Itens.append(produto["quantidade"]) 
-        
-        # Funcao para somar todos os itens da lista total_itens e printando o valor no console.
-    print(f"O total de itens no seu estoque atualmente é de: {sum(total_Itens)} itens.") 
+        # Realizando a soma da quantidade dos itens.
+        total_Itens += produto["quantidade"] 
+    print(f"O total de itens no seu estoque atualmente é de: {total_Itens} itens.") 
     
-def cadastrar_Produto():
-    
-    # Criando uma lista com os codigos existentes.
-    codigos_verificados = []
+def atualizar_Lista_Codigos():
+    # Atualizando a lista de codigos com os produtos cadastrados.
     for produto in produtos_Cadastrados:
         codigos_verificados.append(produto["codigo"])
 
+def verifica_Codigo(codigo):
+    # Retorna True se o código NAO estiver utilizado.
+    return codigo not in codigos_verificados
+def solicitar_Codigo():
+    while True:
+        
+        try: 
+            c = int(input("Digite o código de barras deste produto: "))
+        except ValueError:
+            print("Por favor, digite apenas números")
+            continue
+        if verifica_Codigo(c):
+            return c
+        else:
+            print("Este código já está em uso! Verifique e digite novamente.")
+
+def validar_Valor(valor):
+    # Retorna True se o valor for maior que zero.
+    return valor > 0
+def solicitar_Valor():
+    # Solicita ao usuário um valor.
+    while True:
+        try:
+            v = float(input("Digite agora o valor deste produto: ").replace(",", "."))
+        except ValueError:
+            print("Por favor, digite apenas números.")
+            continue
+        if validar_Valor(v):
+            return v
+        print("Valor inválido! Verifique e digite novamente.")
+
+def validar_Quantidade(quantidade):
+    # Retornará True se a quantidade for maior ou igual a zero.
+    return quantidade >= 0
+def solicitar_Quantidade():
+    # Solicitando ao usuário a quantidade do produto.
+    while True:
+        try:
+            q = int(input("Digite, por fim, a quantidade deste produto: "))
+        except ValueError:
+            # Caso o usuário digite algo que nao seja um numero, o programa emitirá um aviso e repetirá a pergunta.
+            print("Por favor, digite apenas números.")
+            # O comando continue faz com que o loop se repita, voltando para o início do while, sem executar o restante do código abaixo.
+            continue
+        if validar_Quantidade(q):
+            return q
+
+def cadastrar_Produto():
+
+    atualizar_Lista_Codigos()
+
     # Nome do produto a ser adicionado.
     nome = str(input("Digite o nome do produto que deseja adicionar: "))
-    
-    # Verificador de código existente
-    while True:
-        # Codigo do produto a ser adicionado.
-        codigo = int(input("Digite agora o código de barras deste produto: "))
-        # Caso o codigo NAO for existente, o mesmo será adicionado e o programa continua
-        if not codigo in codigos_verificados:
-            break
-        # Caso o codigo for existente, o loop se repete.
-        print("Código inválido ou já está cadastrado!")
-    
-
-    # Verificador de valor valido.
-    while True:
-        # Valor de produtos a ser adicionado
-        valor = float(input("Digite agora o valor deste produto: ").replace(",", "."))
-        # Se o produto valer mais ou igual a 0 o programa segue.
-        if valor > 0:
-            break
-        # Caso o valor digitado seja invalido o loop se repete.
-        print("Valor invalido!")
-
-    # Verificador de quantidade valida
-    while True:
-        # Quantidade de produtos a ser adicionado
-        quantidade = int(input("Digite, por fim, a quantidade deste produto: "))
-        # Se o produto for maior ou igual a 0 o mesmo concluira o cadastro.
-        if quantidade >= 0:
-            break
-        # Caso a quantidade digitada seja invalida o loop se repete.
-        print("Quantidade invalida!")
-
+    codigo = solicitar_Codigo()
+    valor = solicitar_Valor()
+    quantidade = solicitar_Quantidade()
 
     # Transforma as informacoes em dicionario
     produto = {"nome": nome, "codigo": codigo , "valor": valor, "quantidade": quantidade}
@@ -71,37 +88,48 @@ def cadastrar_Produto():
 
     print("Produto cadastrado com sucesso!")
 
+    # Limpa a lista de verificacao do código após o cadastro realizado, evitando duplicatas de códigos em futuras verificações.
+    codigos_verificados.clear()
+
 def exibir_Itens():
-    for produtos in produtos_Cadastrados:
-        print(produtos, sep='\n')
+    
+    # Percorrendo os itens cadastrados.
+    for produto in produtos_Cadastrados:
+        # Criando uma mensagem formatada adicionando a chave do dicionario correspondente a informacao a ser exibida.
+        mensagem = f"""Nome: {produto["nome"]};
+Código: {produto["codigo"]};
+Valor: R$ {produto["valor"]:.2f}:
+Quantidade: {produto["quantidade"]}.
+
+==========================
+"""
+        print(mensagem)
 
 def exibir_Menu():
 
-    print("O que voce gostaria de fazer?")
-    mensagem = """
-    [1] - Exibir itens em estoque.
-    [2] - Cadastrar novo produto.
-    [3] - Exibir quantidade total de itens em estoque.
-    
-    
-    [0] - Fechar programa.
-"""
-    print(mensagem)
-    menu = int(input("Digite aqui o numero desejado: "))
-    match menu:
-        case 1:
-            exibir_Itens()
-        case 2:
-            cadastrar_Produto()
-        case 3:
-            calcular_Total()
-        case 0: 
-            return True
-        case _:
-            print("Digite um dos valores correspondentes ao que deseja!")
+    while True:
+        print("O que voce gostaria de fazer?")
+        mensagem = """
+        [1] - Exibir itens em estoque.
+        [2] - Cadastrar novo produto.
+        [3] - Exibir quantidade total de itens em estoque.
+        
+        
+        [0] - Fechar programa.
+    """
+        print(mensagem)
+        menu = int(input("Digite aqui o numero desejado: "))
+        match menu:
+            case 1:
+                exibir_Itens()
+            case 2:
+                cadastrar_Produto()
+            case 3:
+                calcular_Total()
+            case 0: 
+                break
+            case _:
+                print("Digite um dos valores correspondentes ao que deseja!")
 
-    return False
-
-while True:
-    if exibir_Menu():
-        break
+exibir_Menu()
+input("Pressione Enter para finalizar")
